abbf8a21e2c25a15a1d474f2107f0436e2346350
David Furby <57434673+Davidfurby@users.noreply.github.com>
2021-02-01 13:45:22 +0100

    Test

--- src/main/java/com/backend/backend/BackendApplication.java
+++ src/main/java/com/backend/backend/BackendApplication.java
@@ -2,11 +2,22 @@ package com.backend.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+
+	@RestController
+	class TestController {
+		@GetMapping("/")
+		String hello() {
+			return "Hello world"; 
+		}
 	}
 }
